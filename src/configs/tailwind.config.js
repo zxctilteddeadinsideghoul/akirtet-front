600f3daae3da7e9eb5feac395c1bbfe303d53ac4
sadtowa <toni.afanasev.0505@mail.ru>
2025-02-04 05:44:59 +0500

    auth page

--- src/configs/tailwind.config.js
+++ src/configs/tailwind.config.js
@@ -3,11 +3,13 @@
 export default {
   theme: {
     colors:{
+      mywhite: "#FFFFFF",
       lightgray: "#F8F8F9",
       customgreen: "#7CDABF",
       primaryblue: "#91C9E4",
       secondaryblue: "#4782AA",
       dangerblue: "#C5DCEC",
+      twgray: "#F0F0F0"
     }
   }
 }
